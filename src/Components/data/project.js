2cc703fd0fe0b7b6930bd54c0c3250fc638c3130
AbhishekKasera7696 <akasera62@gmail.com>
2022-05-18 18:14:41 +0530

    newupdate

--- src/Components/data/project.js
+++ src/Components/data/project.js
@@ -1,13 +1,14 @@
 export const ProjectData = [
     {
-        id: 1,
-        title:"Fitmeals",
-        about: "Fitmeals.co.in is an online food delivering website, which deals with wholesome & nourishing foods that make people feel the best version of themself. Areas of responsibility: Developed the entire homepage of the Fitmeals.co.in. A collaborative project, built in 5 days by a team of 4 developers.",
-        tags: ["HTML" , "Css", "Javscript"],
-        demo: "https://featmealclone.netlify.app/",
-        github: "https://github.com/AbhishekKasera7696/FitMeal",
-        image : "https://i.ibb.co/qs7HYFM/Capture-fitmeals.png"
+        id:1,
+        title:"Video Chat App",
+        about: "Features : Users can do video calls by sharing their codes and have fun with each other this app also ask you to give permission for microphone and video",
+        tags: ["React_JS", "Material_UI" , "WebRTC", "NodeJS", "Socket.io", "ExpressJS"],
+        demo: "https://abhishekmychatapp.netlify.app/"  ,
+        github: "https://github.com/AbhishekKasera7696/video_chat_app",
+        image: "https://i.ibb.co/4NVJgxT/Screen-Shot-2020-03-25-at-11-14-30-AM.jpg"
     },
+
 
     {
         id: 2,
@@ -19,14 +20,15 @@ export const ProjectData = [
         image : "https://i.ibb.co/hyPJDKJ/Capture-youtube.png"
     },
 
+   
     {
-        id:3,
-        title:"Video Chat App",
-        about: "Features : Users can do video calls by sharing their codes and have fun with each other this app also ask you to give permission for microphone and video",
-        tags: ["React_JS", "Material_UI" , "WebRTC", "NodeJS", "Socket.io", "ExpressJS"],
-        demo: "https://abhishekmychatapp.netlify.app/"  ,
-        github: "https://github.com/AbhishekKasera7696/video_chat_app",
-        image: "https://i.ibb.co/4NVJgxT/Screen-Shot-2020-03-25-at-11-14-30-AM.jpg"
+        id: 3,
+        title:"Fitmeals",
+        about: "Fitmeals.co.in is an online food delivering website, which deals with wholesome & nourishing foods that make people feel the best version of themself. Areas of responsibility: Developed the entire homepage of the Fitmeals.co.in. A collaborative project, built in 5 days by a team of 4 developers.",
+        tags: ["HTML" , "Css", "Javscript"],
+        demo: "https://featmealclone.netlify.app/",
+        github: "https://github.com/AbhishekKasera7696/FitMeal",
+        image : "https://i.ibb.co/qs7HYFM/Capture-fitmeals.png"
     },
 
     {
